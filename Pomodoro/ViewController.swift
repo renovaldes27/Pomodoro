35fa71221dd784e570ec0f38bca05be93e893f3f
renovaldes <renovaldes@email.arizona.edu>
2017-12-18 14:02:30 -0700

    first commit

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var seconds = 1500 //This variable will hold a starting value of seconds. It could be any amount above 0.
+    var timer = Timer()
+    var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
 
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if(!isTimerRunning){
+            runTimer()
+            isTimerRunning = true
+        }
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        self.seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = timeString(time: TimeInterval(self.seconds)) //This will update the label.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
