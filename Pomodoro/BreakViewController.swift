e378e8f136c9d091f2f8033315872901e34dd554
renovaldes <renovaldes@email.arizona.edu>
2017-12-23 15:21:53 -0700

    Fixed button resizing bug

--- Pomodoro/BreakViewController.swift
+++ Pomodoro/BreakViewController.swift
@@ -9,11 +9,50 @@
 import UIKit
 
 class BreakViewController: UIViewController {
-
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var seconds = 300 //This variable will hold a starting value of seconds. It could be any amount above 0.
+    var timer = Timer()
+    var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        if(!isTimerRunning){
+            runTimer()
+            isTimerRunning = true
+        }
+    }
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        self.seconds = 300
+        isTimerRunning = false
+        timerLabel.text = "05:00"
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: (#selector(WorkViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        self.seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = timeString(time: TimeInterval(self.seconds)) //This will update the label.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 4
+        resetButton.layer.cornerRadius = 4
+        
+        startButton.frame = CGRect(x: 30, y: 30, width: 150, height: 150)
     }
 
     override func didReceiveMemoryWarning() {
