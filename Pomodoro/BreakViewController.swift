590f8133f587a8a08564a77ddf14050da86ac9f5
renovaldes <renovaldes@email.arizona.edu>
2017-12-22 19:09:03 -0700

    Added workView and started breakView

---
+++ Pomodoro/BreakViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BreakViewController.swift
+//  Pomodoro
+//
+//  Created by Reno Valdes on 12/22/17.
+//  Copyright © 2017 Reno Valdes. All rights reserved.
+//
+
+import UIKit
+
+class BreakViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pomodoro/WorkViewController.swift
+++ Pomodoro/WorkViewController.swift
@@ -8,25 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WorkViewController: UIViewController {
     
-    
-    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
     
     var seconds = 1500 //This variable will hold a starting value of seconds. It could be any amount above 0.
     var timer = Timer()
     var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
 
     
-    
-    @IBAction func startButtonTapped(_ sender: UIButton) {
+    @IBAction func startButtonTapped(_ sender: Any){
         if(!isTimerRunning){
             runTimer()
             isTimerRunning = true
         }
     }
     
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        self.seconds = 1500
+        isTimerRunning = false
+        timerLabel.text = "25:00"
+    }
+
     func timeString(time:TimeInterval) -> String {
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
@@ -35,7 +41,7 @@ class ViewController: UIViewController {
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self,
-                selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+                selector: (#selector(WorkViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
@@ -45,7 +51,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startButton.layer.cornerRadius = 4
+        resetButton.layer.cornerRadius = 4
+        
+        startButton.frame = CGRect(x: 30, y: 30, width: 150, height: 150)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
